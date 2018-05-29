5cabe2df908bfa76d48efba92a98c95ddef212d6
github username <github email address>
2018-05-29 17:54:08 -0400

    cleaned up

--- pongbot.rb
+++ pongbot.rb
@@ -2,21 +2,12 @@
 require 'slack-ruby-bot'
 require 'pry'
 
-
+DROID_TODO = "I have your TODO list right here. What would you like me to do with it?? \n1. View List\n2. Add to List\n3. Delete From List"
 class PongBot < SlackRubyBot::Bot
   command 'droid' do |client, data, match|
     client.say(text: "What do you want?!", channel: data.channel)
     sleep(2)
     client.say(text: "1. Weather\n 2. Stock\n 3. TODO List", channel: data.channel)
-    if command '1' do |client, data, match|
-      #weather
-    elsif command '2' do |client, data, match|
-      #stock
-    elsif command '3' do |client, data, match|
-      #TO DO
-    else
-      #quit droid helper
-    end
   end
 end
 
